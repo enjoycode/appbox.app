d8e1e349c73017a1f3d274f9a82fda19f39f5081
enjoycode <enjoycode@icloud.com>
2020-02-17 16:40:37 +0800

    同步dev

--- src/assets/js/Runtime.js
+++ src/assets/js/Runtime.js
@@ -1,3 +1,5 @@
+import { Entity } from './Entity'
+
 // 参考: https://github.com/airyland/vux/blob/v2/src/plugins/device/index.js
 const _isAndroid = /(Android);?[\s/]+([\d.]+)?/.test(navigator.userAgent)
 const _isIpad = /(iPad).*OS\s([\d_]+)/.test(navigator.userAgent)
@@ -20,6 +22,41 @@ export default {
      * 用于预览窗口判断是否正在设计时
      */
     isDevelopment() { return this._isDevelopment },
+
+    /** 将数据对象转为实体(代理) */
+    parseEntity(root) {
+        const typeProp = '$T'
+
+        //TODO:考虑js Proxy方案，另考虑channel的Protocol
+        function walk(it) {
+            if (typeof it !== 'object' || !it || it instanceof Date) {
+                return it
+            }
+            if (Array.isArray(it)) {
+                for (let i = 0; i < it.length; i++) {
+                    it[i] = walk(it[i])
+                }
+            } else {
+                //判断是否实体
+                if (it.hasOwnProperty(typeProp) && typeof it[typeProp] === 'string' && !isNaN(it[typeProp].charAt(0))) {
+                    it = Object.assign(new Entity(""), it)
+                }
+                for (const key in it) {
+                    it[key] = walk(it[key])
+                }
+            }
+            return it
+        }
+
+        return walk(root)
+    },
+
+    /** 根据模型标识新建实体实例 */
+    newEntity(modelId, obj) {
+        return Object.assign(new Entity(modelId), obj)
+    },
+
+    //TODO: 提供require([js1, js2])加载外部js
 
     /** 获取微信JS SDK */
     getWxSdk() {
